7996b7204aa1b74f158bdf1237c364be330b3861
admin <admin>
2021-04-20 01:22:40 +0800

    冲突

--- hello/src/hello/test/Test.java
+++ hello/src/hello/test/Test.java
@@ -3,8 +3,6 @@ package hello.test;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello");
 		System.out.println("hello");
 		System.out.println("hello");
 	}
