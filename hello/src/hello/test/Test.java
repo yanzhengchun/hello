345b7867858965630e85c3ec711688dcffda070d
yanzhengchun <82820247+yanzhengchun@users.noreply.github.com>
2021-04-20 01:28:31 +0800

    Update Test.java

--- hello/src/hello/test/Test.java
+++ hello/src/hello/test/Test.java
@@ -3,8 +3,8 @@ package hello.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("hello");
+		System.out.println("hello111");
+		System.out.println("hello222");
 		int i = 0;
 	}
 
